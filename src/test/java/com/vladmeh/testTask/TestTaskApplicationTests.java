6b7f39a3e2d5da18b4609731350687ce8263c8c1
VladMeh <vladmeh@gmail.com>
2017-10-18 17:55:39 +0300

    testing

--- src/test/java/com/vladmeh/testTask/TestTaskApplicationTests.java
+++ src/test/java/com/vladmeh/testTask/TestTaskApplicationTests.java
@@ -1,5 +1,6 @@
 package com.vladmeh.testTask;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
